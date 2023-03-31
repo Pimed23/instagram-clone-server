bca40fa100a045f1a7709606b784a0cca3b510d0
Pimed23 <b.pimed@gmail.com>
2023-03-31 11:39:35 -0500

    feat: adding jwt token

---
+++ migrations/1680279934357-initialMigration.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class initialMigration1680279934357 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, Router } from 'express';
 import userService from '../services/userService';
+import jwt from 'jsonwebtoken';
 
 const userRouter = Router();
 
@@ -7,11 +8,16 @@ userRouter.post('/auth', async (req: Request, res: Response, next) => {
   try {
     const {username, password} = req.body;
     const user = await userService.findUser(username, password);
-    res.send(user);
+    const accessToken = generateAccessToken(user.username);
+    res.json({token: accessToken});
   } catch (err) {
     console.error(err);
     res.status(404).json('Something went wrong...')
   } 
 });
+
+const generateAccessToken = (user) => {
+	return jwt.sign({name: user}, process.env.SECRET, {expiresIn: 5});
+}
 
 export default userRouter;
